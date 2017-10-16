f0e6bd605ba7b87a2915ddb3534911704605568a
miyazi777 <m71203@gmail.com>
2017-10-16 23:09:43 +0900

    rensai4

--- vue-memo/src/App.vue
+++ vue-memo/src/App.vue
@@ -1,23 +1,60 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <editor-view @add="add"></editor-view>
+    <list-view :memos="memos" @remove="remove"></list-view>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  import EditorView from './components/EditorView'
+  import ListView from './components/ListView'
+
+  export default {
+    name: 'app',
+    data() {
+      return {
+        memos: [
+          {
+            id: 1,
+            text: 'test',
+            date: '2017-10-23',
+            tags: ['tag1', 'tag2']
+          },
+          {
+            id: 2,
+            text: 'test2',
+            date: '2017-11-23',
+            tags: ['tag1', 'tag2']
+          }
+        ]
+      }
+    },
+    computed: {
+      nextId() {
+        return this.memos.reduce((id, memo) => {
+          return id < memo.id ? memo.id : id
+        }, 0) + 1
+      }
+    },
+    methods: {
+      add(newMemo) {
+        newMemo.id = this.nextId
+        this.memos.push(newMemo)
+      },
+      remove(id) {
+        const index = this.memos.findIndex((memo) => {
+          return memo.id === id
+        })
+        this.memos.splice(index,1)
+      }
+    },
+    components: {
+      'editor-view': EditorView,
+      'list-view': ListView
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import '../static/styles.css'
 </style>
